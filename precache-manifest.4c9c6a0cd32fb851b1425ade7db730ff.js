28b694e8f8a710433a882d034a0ae60d5bed182d
kartheekl68 <kartheekl68@gmail.com>
2021-01-04 15:44:29 -0600

    Updates

---
+++ precache-manifest.4c9c6a0cd32fb851b1425ade7db730ff.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec6324414b08fd13bf76f4d4879defa6",
+    "url": "/NewsApp/index.html"
+  },
+  {
+    "revision": "bec35df0ccea5d02fbb1",
+    "url": "/NewsApp/static/css/main.5a8441c2.chunk.css"
+  },
+  {
+    "revision": "d88f5ee4fa9252db0dd6",
+    "url": "/NewsApp/static/js/2.5e10f134.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/NewsApp/static/js/2.5e10f134.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bec35df0ccea5d02fbb1",
+    "url": "/NewsApp/static/js/main.8b0e4647.chunk.js"
+  },
+  {
+    "revision": "348c7eb888d9888f4551",
+    "url": "/NewsApp/static/js/runtime-main.b5cbe6a3.js"
+  },
+  {
+    "revision": "abba636d53e3eb6e2d4f8d1798a843c4",
+    "url": "/NewsApp/static/media/culture.abba636d.jpg"
+  },
+  {
+    "revision": "3e746068cea54e50643bfb74d738b31a",
+    "url": "/NewsApp/static/media/github.3e746068.svg"
+  },
+  {
+    "revision": "baaaeb85a4c7db956d2104c3ea6feca9",
+    "url": "/NewsApp/static/media/instgram.baaaeb85.svg"
+  },
+  {
+    "revision": "94c6af720dde760e97b6da2a817a0136",
+    "url": "/NewsApp/static/media/life_style.94c6af72.jpg"
+  },
+  {
+    "revision": "ca4020c11a19026696fa3dabbd22f284",
+    "url": "/NewsApp/static/media/logo-name.ca4020c1.svg"
+  },
+  {
+    "revision": "64d3d1ec0e818355000a4a6b54a8190e",
+    "url": "/NewsApp/static/media/movies.64d3d1ec.jpg"
+  },
+  {
+    "revision": "5af2abb4cf728e3fe98d5f9b3b17c507",
+    "url": "/NewsApp/static/media/music.5af2abb4.jpg"
+  },
+  {
+    "revision": "7ff38406a6739a5e4aeb4d0cf8ca5292",
+    "url": "/NewsApp/static/media/news.7ff38406.png"
+  },
+  {
+    "revision": "7d5e821d46a4ced26a49b0f3e49c8886",
+    "url": "/NewsApp/static/media/open-menu.7d5e821d.svg"
+  },
+  {
+    "revision": "b3b37b993832d475d3dd77ce174cbaa4",
+    "url": "/NewsApp/static/media/search.b3b37b99.svg"
+  },
+  {
+    "revision": "e1c451302774ac86807484ce29cb0801",
+    "url": "/NewsApp/static/media/technology.e1c45130.jpg"
+  },
+  {
+    "revision": "b4fae7c81954fef00ce5265316fc7ebb",
+    "url": "/NewsApp/static/media/twitter.b4fae7c8.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/NewsApp/precache-manifest.4c9c6a0cd32fb851b1425ade7db730ff.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/NewsApp/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
